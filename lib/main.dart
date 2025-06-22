138268753456a235a1544cb7c175e2b6b6401712
jmacabrera <jmacabrera@poligran.edu.co>
2025-06-22 13:54:15 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:patania_app/registro_form.dart';
-import 'package:patania_app/servicios_screen.dart';
-import 'package:patania_app/trofeos_actividad.dart';
-import 'package:patania_app/trofeos_alimentacion.dart';
-
+import 'package:patania_app/home_pr.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive); 
-
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
   runApp(PataniaApp());
 }
 
@@ -18,8 +13,13 @@ class PataniaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
       debugShowCheckedModeBanner: false,
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/home': (context) => HomeScreen(),
+        '/registro': (context) => RegisterFormScreen(),
+      },
     );
   }
 }
@@ -27,113 +27,115 @@ class PataniaApp extends StatelessWidget {
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    backgroundColor: Color(0xFFF3EAEA),
-    body: SafeArea(
-      child: Container(
-        width: double.infinity,
-        height: double.infinity,
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        child: SingleChildScrollView(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              minHeight: MediaQuery.of(context).size.height,
-            ),
-            child: IntrinsicHeight(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(height: 40),
-                  Text(
-                    'Patania',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-                  ),
-                  SizedBox(height: 16),
-                  CircleAvatar(
-                    radius: 40,
-                    backgroundColor: Colors.white,
-                    child: Icon(Icons.pets, size: 40, color: Colors.black),
-                  ),
-                  SizedBox(height: 24),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Email',
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8)),
+    return Scaffold(
+      backgroundColor: Color(0xFFF3EAEA),
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: MediaQuery.of(context).size.height,
+              ),
+              child: IntrinsicHeight(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 40),
+                    Text(
+                      'Patania',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
                     ),
-                  ),
-                  SizedBox(height: 16),
-                  TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: 'Contraseña',
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8)),
+                    SizedBox(height: 16),
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Colors.white,
+                      child: Icon(Icons.pets, size: 40, color: Colors.black),
                     ),
-                  ),
-                  SizedBox(height: 8),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text('¿Olvidaste contraseña?',
-                          style: TextStyle(fontSize: 12)),
-                    ),
-                  ),
-                  SizedBox(height: 8),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: Size(double.infinity, 48),
-                      backgroundColor: Colors.black,
-                    ),
-                    child: Text('Iniciar sesión'),
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('¿No tienes cuenta?'),
-                      TextButton(
-                        child: Text('Crea tu cuenta'),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => RegisterFormScreen()));
-                        },
+                    SizedBox(height: 24),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Email',
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
                       ),
-                    ],
-                  ),
-                  SizedBox(height: 8),
-                  Text('O conéctate con'),
-                  SizedBox(height: 8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.mail, size: 30),
+                    ),
+                    SizedBox(height: 16),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: 'Contraseña',
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: TextButton(
                         onPressed: () {},
+                        child: Text('¿Olvidaste contraseña?',
+                            style: TextStyle(fontSize: 12)),
                       ),
-                      SizedBox(width: 16),
-                      IconButton(
-                        icon: Icon(Icons.facebook, size: 30),
-                        onPressed: () {},
+                    ),
+                    SizedBox(height: 8),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/home');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(double.infinity, 48),
+                        backgroundColor: Colors.black,
                       ),
-                    ],
-                  ),
-                  SizedBox(height: 40),
-                ],
+                      child: Text('Iniciar sesión'),
+                    ),
+                    SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('¿No tienes cuenta?'),
+                        TextButton(
+                          child: Text('Crea tu cuenta'),
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/registro');
+                          },
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    Text('O conéctate con'),
+                    SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.mail, size: 30),
+                          onPressed: () {},
+                        ),
+                        SizedBox(width: 16),
+                        IconButton(
+                          icon: Icon(Icons.facebook, size: 30),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 40),
+                  ],
+                ),
               ),
             ),
           ),
         ),
       ),
-    ),
-  );
- }
+    );
+  }
 }
